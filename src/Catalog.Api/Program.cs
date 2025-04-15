1560368a501e1657da66a62c808393ba2da5fa32
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-15 18:38:00 -0300

    feat(Api): configure programCs and create a productController

--- src/Catalog.Api/Program.cs
+++ src/Catalog.Api/Program.cs
@@ -1,18 +1,42 @@
+using Catalog.CrossCutting.Middlewares;
+using Catalog.IoC;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// Swagger configuration
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Catalog API",
+        Version = "v1",
+        Description = "API RESTful para gerenciamento de produtos no catálogo"
+    });
+});
+
+// Register project dependencies
+builder.Services.AddDependencies(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog API V1");
+        c.RoutePrefix = string.Empty;
+    });
 }
+
+// Middleware global de exceção
+app.UseMiddleware<GlobalExceptionMiddleware>();
 
 app.UseHttpsRedirection();
 
@@ -20,4 +44,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+await app.RunAsync();
