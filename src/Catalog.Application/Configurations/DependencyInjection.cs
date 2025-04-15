ca944c1156d91f40a5cfdf98ceee006f0d81b76c
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-15 18:32:27 -0300

    feat(application): Add dependencyinjection

---
+++ src/Catalog.Application/Configurations/DependencyInjection.cs
@@ -0,0 +1,34 @@
+﻿using Catalog.Application.DTOs.Product;
+using Catalog.Application.Mappers.ProductMapper;
+using Catalog.Application.Mappers;
+using Catalog.Domain.Entities;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Catalog.Application.Common.Behaviors;
+using FluentValidation;
+
+namespace Catalog.Application.Configurations
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            // (Handlers)
+            services.AddMediatR(cfg =>
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            // (Behaviors)
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            // (Validators)
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            // (Mappers)
+            services.AddScoped<IMapper<Product, ProductDto>, ProductMapper>();
+
+
+            return services;
+        }
+    }
+}
