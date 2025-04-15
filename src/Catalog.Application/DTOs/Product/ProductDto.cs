ca2506ac2d2472f68d5425aa5642a8f824a26ffb
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-15 18:26:51 -0300

    feat(application): Create a base structure of Register Product

---
+++ src/Catalog.Application/DTOs/Product/ProductDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.DTOs.Product
+{
+    public class ProductDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string Sku { get; set; } = null!;
+        public int Stock { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
