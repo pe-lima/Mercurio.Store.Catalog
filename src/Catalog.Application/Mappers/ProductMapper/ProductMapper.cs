4973fb30751cb3e0ee73a3f9ff91359170e99665
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-15 18:31:14 -0300

    Feat(application): Add Mapper of product

---
+++ src/Catalog.Application/Mappers/ProductMapper/ProductMapper.cs
@@ -0,0 +1,42 @@
+﻿using Catalog.Application.DTOs.Product;
+using Catalog.Domain.Entities;
+using Catalog.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Mappers.ProductMapper
+{
+    public class ProductMapper : IMapper<Product, ProductDto>
+    {
+        public Product ToSource(ProductDto target)
+        {
+            return new Product(
+                target.Name,
+                new Description(target.Description),
+                new Price(target.Price),
+                new Sku(target.Sku),
+                new Stock(target.Stock)
+            );
+        }
+
+        public ProductDto ToTarget(Product source)
+        {
+            return new ProductDto
+            {
+                Id = source.Id,
+                Name = source.Name,
+                Description = source.Description.Value,
+                Price = source.Price.Value,
+                Sku = source.Sku.Value,
+                Stock = source.Stock.Quantity,
+                IsActive = source.IsActive,
+                CreatedAt = source.CreatedAt,
+                UpdatedAt = source.UpdatedAt
+            };
+        }
+
+    }
+}
