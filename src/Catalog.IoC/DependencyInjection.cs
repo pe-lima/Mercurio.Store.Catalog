d94449ad094dd748219eba21958e7fe20fa81fb2
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-15 18:36:17 -0300

    feat(ioc): add DependencyInjection of Application and Infra

---
+++ src/Catalog.IoC/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.Application.Configurations;
+using Catalog.Infrastructure.Configurations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalog.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddApplication();
+            services.AddInfra(configuration);
+
+            return services;
+        }
+    }
+}
