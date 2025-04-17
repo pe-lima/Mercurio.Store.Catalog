e4a2ca5e379c9e7ca7b8a352f5561315334eeb9b
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-17 10:46:11 -0300

    feat(application): add structure of Get All products in application

---
+++ src/Catalog.Application/Handlers/ProductHandler/GetAllProductsQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Catalog.Application.DTOs.Product;
+using Catalog.Application.Mappers;
+using Catalog.Application.Queries.ProductQuery;
+using Catalog.Domain.Entities;
+using Catalog.Domain.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Handlers.ProductHandler
+{
+    public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQuery, List<ProductDto>>
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper<Product, ProductDto> _mapper;
+        public GetAllProductsQueryHandler(IProductRepository productRepository, IMapper<Product, ProductDto> mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+        public async Task<List<ProductDto>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
+        {
+            var products = await _productRepository.GetAllAsync(cancellationToken);
+
+            return [.. products.Select(product => _mapper.ToTarget(product))];
+        }
+    }
+}
