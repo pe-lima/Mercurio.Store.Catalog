a5b9e7f841a27290f8ddc5d567cb040eddd52182
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-17 10:43:04 -0300

    chore(domain): Add CancellationToken in async methods

--- src/Catalog.Domain/Interfaces/Repositories/IRepository.cs
+++ src/Catalog.Domain/Interfaces/Repositories/IRepository.cs
@@ -8,9 +8,9 @@ namespace Catalog.Domain.Interfaces.Repositories
 {
     public interface IRepository<T> where T : class
     {
-        Task<T?> GetByIdAsync(Guid id);
-        Task<IEnumerable<T>> GetAllAsync();
-        Task AddAsync(T entity);
+        Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task AddAsync(T entity, CancellationToken cancellationToken = default);
         void Update(T entity);
         void Remove(T entity);
     }
